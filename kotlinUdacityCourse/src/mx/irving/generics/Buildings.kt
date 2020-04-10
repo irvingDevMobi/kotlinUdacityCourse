08e4a864a0fc37e83c2dd6884b06f4b64ac44672
Irving Lop <ilopezp@gbm.com.mx>
2020-04-10 14:50:45 -0500

    [Udacity Course] L5-Q11 Generics

---
+++ kotlinUdacityCourse/src/mx/irving/generics/Buildings.kt
@@ -0,0 +1,22 @@
+package mx.irving.generics
+
+open class BaseBuildingMaterial(var numberNeeded: Int = 1)
+
+class Wood() : BaseBuildingMaterial(4)
+
+class Brick : BaseBuildingMaterial(8)
+
+class Building<T : BaseBuildingMaterial>(val baseMaterial: T) {
+    val baseMaterialsNeeded = 100
+    val actualMaterialsNeeded: Int
+        get() = baseMaterial.numberNeeded * baseMaterialsNeeded
+
+    fun build() {
+        println("$actualMaterialsNeeded ${baseMaterial::class.simpleName} needed")
+    }
+}
+
+fun main() {
+    val building = Building<Wood>(Wood())
+    building.build()
+}
