458d4a29ecd09c37075695b769c14523e4984cc3
Irving Lop <ilopezp@gbm.com.mx>
2020-04-24 09:38:24 -0500

    [Udacity Course] L6-Q5 Higher-Order Functions

--- kotlinUdacityCourse/src/mx/irving/lesson6/Aquarium6.kt
+++ kotlinUdacityCourse/src/mx/irving/lesson6/Aquarium6.kt
@@ -28,8 +28,30 @@ fun fishExamples() {
             .let { it + "fish" }
             .let { it.length }
             .let { it + 31 })
+
+    val numbers = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    println(numbers.divisorOf3())
+
+    print(numbers.divisibleBy {
+        it.rem(3)
+    })
 }
 
 fun myWith(name: String, block: String.() -> Unit) {
     name.block()
+}
+
+fun List<Int>.divisorOf3(): List<Int> {
+    return filter { it == 0 || it % 3 == 0 }
+}
+
+// Course Solution
+fun List<Int>.divisibleBy(block: (Int) -> Int): List<Int> {
+    val result = mutableListOf<Int>()
+    for (item in this) {
+        if (block(item) == 0) {
+            result.add(item)
+        }
+    }
+    return result
 }
