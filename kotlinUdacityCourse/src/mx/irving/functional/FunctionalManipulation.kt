257281f560e1ce3430ecf018191e310960fb66fa
Irving Lop <ilopezp@gbm.com.mx>
2020-04-24 19:04:32 -0500

    [Udacity Course] L6-Q8 My Game location solution

--- kotlinUdacityCourse/src/mx/irving/functional/FunctionalManipulation.kt
+++ kotlinUdacityCourse/src/mx/irving/functional/FunctionalManipulation.kt
@@ -7,39 +7,89 @@ enum class Directions {
 
 class Game {
     var path = mutableListOf(Directions.START)
+    val northLambda: () -> Boolean = {
+        path.add(Directions.NORTH)
+    }
+    val southLambda: () -> Boolean = {
+        path.add(Directions.SOUTH)
+    }
+
+    val eastLambda: () -> Boolean = {
+        path.add(Directions.EAST)
+    }
+
+    val westLambda: () -> Boolean = {
+        path.add(Directions.WEST)
+    }
+
+    val endLambda: () -> Boolean = {
+        path.add(Directions.END)
+        print("Game Over: ")
+        println(path)
+        path.clear()
+        false
+    }
+
+    fun move(where: () -> Boolean) {
+        where()
+    }
+
+    fun makeMove(move: String?) {
+        when (move) {
+            "n" -> move(northLambda)
+            "s" -> move(southLambda)
+            "e" -> move(eastLambda)
+            "w" -> move(westLambda)
+            else -> move(endLambda)
+        }
+    }
 }
 
-val northLambda: (paths: MutableList<Directions>) -> Any = {
-    it.add(Directions.NORTH)
-}
+class Location(private val width: Int = 4, private val height: Int = 4) {
+    private val map = Array(width) { arrayOfNulls<String>(height) }
+    private var column = 0
+    private var row = 0
 
-val southLambda: (paths: MutableList<Directions>) -> Any = {
-    it.add(Directions.SOUTH)
-}
+    init {
+        map[row][column] = "X"
+    }
 
-val eastLambda: (paths: MutableList<Directions>) -> Any = {
-    it.add(Directions.EAST)
-}
+    fun move(direction: String?) {
+        when (direction) {
+            "n" -> if (row > 0) row--
+            "s" -> if (row < height - 1) row++
+            "w" -> if (column > 0) column--
+            "e" -> if (column < width - 1) column++
+        }
+        map[row][column] = "X"
+    }
 
-val westLambda: (paths: MutableList<Directions>) -> Any = {
-    it.add(Directions.WEST)
-}
-
-val endLambda: (paths: MutableList<Directions>) -> Boolean = {
-    it.add(Directions.END)
-    print("Game Over: ")
-    println(it)
-    it.clear()
-    false
+    fun print() {
+        println()
+        for (line in map) {
+            for (cell in line) {
+                if (cell == null) print("  ") else print("$cell ")
+            }
+            println()
+        }
+    }
 }
 
 fun main() {
     val game = Game()
     println(game.path)
-    northLambda(game.path)
-    southLambda(game.path)
-    eastLambda(game.path)
-    westLambda(game.path)
-    endLambda(game.path)
+    game.northLambda()
+    game.southLambda()
+    game.eastLambda()
+    game.westLambda()
+    game.endLambda()
     println(game.path)
+    val location = Location()
+    while (true) {
+        print("Enter a direction: n/s/e/w:")
+        val move = readLine()
+        game.makeMove(move)
+        location.move(move)
+        location.print()
+    }
 }
